955b5302af071117909d2e245755b947c646bac8
Sergio0831 <sergejs.ivcenko@gmail.com>
2023-05-25 11:52:19 +0100

    Resolved error

--- app/components/Header/Logo.tsx
+++ app/components/Header/Logo.tsx
@@ -3,25 +3,6 @@
 import React from 'react';
 import classes from './Logo.module.scss';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
-import logoPaths from '@/data/logoPaths';
-
-const logoVariants = {
-	hidden: {
-		//opacity: 0,
-		pathLength: 0,
-		fill: 'none',
-	},
-	visible: {
-		//opacity: 1,
-		pathLength: 1,
-		fill: 'currentColor',
-		transition: {
-			duration: 2,
-			ease: 'easeInOut',
-		},
-	},
-};
 
 const Logo = () => {
 	return (
